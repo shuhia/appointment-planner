e09d907a7df96c9e0104a9c9e2c51fba03a4949c
Alex On <shuhia.on@gmail.com>
2021-08-30 22:14:24 +0200

    added fake data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,48 @@ import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+import faker from "faker";
+import { nanoid, random } from "nanoid";
+
+function createFakeContact(amount = 1) {
+  return [...Array(amount)].map(() => {
+    return {
+      id: nanoid(),
+      name: faker.name.firstName(),
+      phone: faker.phone.phoneNumber(),
+      email: faker.internet.email(),
+    };
+  });
+}
+
+function getRandomElementFromArray(array) {
+  if (array) {
+    const length = array.length;
+    const randomIndex = Math.floor(Math.random * length);
+    return array[randomIndex];
+  }
+}
+
+function createFakeAppointment(amount = 1, contact) {
+  return [...Array(amount)].map(() => {
+    return {
+      id: nanoid(),
+      contact,
+      time: faker.time.recent(),
+    };
+  });
+}
 
 function App() {
   /*
   Define state variables for 
   contacts and appointments 
   */
-  const [contacts, setContacts] = useState([]);
-  const [appointments, setAppointments] = useState([]);
+
+  const [contacts, setContacts] = useState(createFakeContact(5));
+  const [appointments, setAppointments] = useState(
+    createFakeAppointment(3, getRandomElementFromArray(contacts))
+  );
 
   const ROUTES = {
     CONTACTS: "/contacts",
